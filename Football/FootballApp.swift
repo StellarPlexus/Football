49e5bef36bb6ba09fe1667830d9071f6c9b149db
Stellarplexus <Stellarplexuses@gmai.com>
2025-02-10 14:54:32 -0600

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Michael Killebrew on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
